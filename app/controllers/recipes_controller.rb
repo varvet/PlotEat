256930376426c4ee5cf80c74472cd74e6a5a5e45
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2019-04-09 23:19:52 +0200

    Add recipe view files Modify recipe model Add methods to controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,13 @@
 class RecipesController < ApplicationController
   def index
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  
 end
---
+++ app/models/recipe.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+class Recipe < ApplicationRecord
+    validates_precence_of title: description:
+end
