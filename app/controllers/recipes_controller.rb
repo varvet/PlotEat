afa2b4a7d17ba72f892484d6d549e1a47fa21d21
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2019-04-10 08:55:31 +0200

    Add create method to controller Add display to recipes show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,9 +5,25 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipie = Recipe.new(recipe_params)
+    if @recipie.save
+      flash[:success] = "Recipe successfully created"
+      redirect_to recipe_path(@recipie)
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
   def show
     @recipe = Recipe.find(params[:id])
   end
   
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+  end
   
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Recipe, type: :model do
   it { is_expected.to have_db_column :title }
   it { is_expected.to have_db_column :description }
+
 end
