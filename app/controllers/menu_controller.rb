4d42ccd9cb53a09be1d7815d8a085e4f47db63ac
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2019-04-23 11:42:31 +0200

    Add menu model Add foreign key association recipe/menu update menu controller

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class MenuController < ApplicationController
+  def def(_update)
+    @menu = Menu.find(params[:id])
+    if @menu.update(params[@menu])
+      flash[:success] = "Menu was successfully updated"
+      redirect_to @menu
+    else
+      flash[:error] = "Something went wrong"
+      render "edit"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_155348) do
+ActiveRecord::Schema.define(version: 2019_04_23_092936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,20 @@ ActiveRecord::Schema.define(version: 2019_04_15_155348) do
     t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
+  create_table "menus", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "menu_id"
+    t.index ["menu_id"], name: "index_recipes_on_menu_id"
   end
 
   add_foreign_key "ingredients", "recipes"
+  add_foreign_key "recipes", "menus"
 end
