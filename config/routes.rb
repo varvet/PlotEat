b189aef2c467918931f3ed5870ff32dd4532d9c3
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2019-05-01 22:41:42 +0200

    Add session route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
     resources :ingredients, only: [:create]
   end
   resources :menus, only: %i[show update]
+
   resources :users, only: %i[new create]
   get "/sign_up", to: "users#new", as: :sign_up
+
+  resources :sessions, only: %i[new create destroy]
+  get "/log_in", to: "sessions#new", as: :log_in
+  delete "/log_out", to: "sessions#destroy", as: :log_out
 end
