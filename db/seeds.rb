5951da87382644bc363f66cd10f011e6fbfb4392
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2019-05-02 13:19:34 +0200

    Add users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(
+  email: "groot@theguardians.com",
+  password: "iamgroot",
+)
+
+User.create(
+  email: "rocket@theguardians.com",
+  password: "watsaraccoon",
+)
+
+
